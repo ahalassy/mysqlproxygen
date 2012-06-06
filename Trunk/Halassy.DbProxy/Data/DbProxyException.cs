d9888ca47e999f6630cbcdc206f7ccc8e7ac4cf4
Adam Halassy <adam.halassy@gmail.com>
2012-06-06 07:30:57 +0200

    Fix brought manually

--- Trunk/Halassy.DbProxy/Data/DbProxyException.cs
+++ Trunk/Halassy.DbProxy/Data/DbProxyException.cs
@@ -26,7 +26,7 @@ namespace Halassy.Data
 		public string RoutineName { get; private set; }
 		
 		public DbProxyException (string routineName, DbStoredRoutineParmCollection parms, Exception ex )
-			:base(String.Format("Error calling \"{0}\"! Message was \"{0}\".", routineName, ex.Message), ex)
+			:base(String.Format("Error calling \"{0}\"! Message was \"{1}\".", routineName, ex.Message), ex)
 		{
 			this.Parameters = parms;
 			this.RoutineName = routineName;
